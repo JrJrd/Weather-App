d16e9e8833c714c4ba277afad12d72aadb682379
seaer <jrd.seaer@gmail.com>
2023-09-16 03:30:59 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const API_KEY = '';
+    const displayWindow = document.getElementById("display");
+    const Bookmarklist = document.getElementById("list");
+    const reloadBtn = document.getElementById("reload");
+
+});
